383e79037a37da909fed6b2a95d6aacac3c45060
raining <135791260+Arainf@users.noreply.github.com>
2024-10-03 13:25:44 +0800

    on progress side and top bar

--- src/components/common/navigatin-side-top/NavigationTop.jsx
+++ src/components/common/navigatin-side-top/NavigationTop.jsx
@@ -1,10 +1,49 @@
+import { useEffect, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import './navigationside.css';
 
-const NavigationTop = () => {
+const NavigationTop = (onSidebarToggle) => {
+  const [profile, setProfile] = useState({
+    imageUrl: "", // Will be dynamically fetched
+    name: "Adrian Rainier Fabian",
+    email: "220622@adzu.edu.ph",
+  });
+
   return (
-    <div>
-
+    <div className="flex p-0 justify-between flex-row items-center overflow-hidden w-screen text-white h-14" style={{backgroundColor: '#001932'}}>
+      <div className="flex flex-row items-center">
+        <div className="menu flex flex-row items-center mx-5 menu" onClick={onSidebarToggle}>
+          <GiHamburgerMenu />
+        </div>
+      </div>
+      <div>
+        <div className="profile">
+          {/* profile section */}
+          <div className="profile_primary flex flex-row relative items-center justify-start h-14 w-auto p-5 no-underline text-white">
+            <img src={profile.imageUrl} alt="Profile" className="profile-img w-12 h-10  " />
+            <div className="profile-details flex justify-between flex-row items-center w-screen">
+              <div className="profile-text flex flex-col ">
+                <h4 className="profile-name">{profile.name}</h4>
+                <p className="profile-email ">{profile.email}</p>
+              </div>
+              {/* <button
+                  // onClick={handleLogout}
+                  className='profile-logout-button'>
+                  <SlLogout />
+                </button> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
 
-export default NavigationTop
+    // {/* Spinner overlay */}
+    // {loading && (
+    //   <div className="spinner-overlay">
+    //     <div className="loader"></div>
+    //   </div>
+    // )}
+  );
+};
+
+export default NavigationTop;
