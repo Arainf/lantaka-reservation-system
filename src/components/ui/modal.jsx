1973963a30313c6fa0b71db4de28b891d57f6380
robitc911 <220450@gmail.com>
2024-10-16 02:41:56 +0800

    (SAVE) modal

--- src/components/ui/modal.jsx
+++ src/components/ui/modal.jsx
@@ -6,19 +6,17 @@ const Modal = ({ isOpen, onClose, onSubmit, reservationData, handleChange }) => 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center text-black ">
-        <div className="bg-white rounded-lg shadow-md p-8 w-6/12 h-5/6 relative">
-          <div className="w-[600px] flex flex-column ">
-          <button 
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center text-black">
+        <div className="bg-white rounded-lg sh2adow-md w-6/12 h-5/6 relative">
+        <button 
             onClick={onClose} 
-            className="flex hover:text-red-500 place-self-end">
-            <X size={24} />
+            className="absolute top-2 right-2 hover:text-red-500 p-0 ">
+            <X size={28} />
           </button>
-          <h2 className="flex text-3xl font-semibold mb-4 ">Add Reservation </h2>
+          <div className="pl-12 pt-12 flex flex-column w-100%">
+          <h2 className="flex text-3xl font-semibold ">Add Reservation </h2>
           </div>
-        </div>
-
-         
+        </div>      
       </div>
   );
 };
